84b1f1ebe9737b894f3584a0e92bc5e20ab61446
S-H-GAMELINKS <gamelinks007@gmail.com>
2018-04-06 17:33:46 +0900

    fix routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :counttypes do
-    collection {post :import}
-  end
-
-  resources :counts do
-    collection {post :import}
-  end
-
-  resources :foods do
-    collection {post :import}
-  end
-
-  resources :foodtypes　do
-    collection {post :import}
-  end
-
-  resources :placetypes do
+  resources :foods, :placetypes, :foodtypes, :counttypes, :counts do
     collection {post :import}
   end
 
   root 'foods#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
 end
